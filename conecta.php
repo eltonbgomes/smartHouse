6aca49fd18656e86689a5386e226e357aecddfb0
Elton Barbosa Gomes <eltonbarbosagomes@gmail.com>
2021-02-07 00:58:49 -0300

    atualizando o .gitignore

--- conecta.php
+++ conecta.php
@@ -3,15 +3,12 @@
 //mysql_set_charset('utf8');
 //ini_set('default_charset','UTF-8');
 
-$hostname_conexao1 = "localhost";
-$database_conexao1 = "smarthouse";
-$username_conexao1 = "root";
-$password_conexao1 = "";
+// $hostname_conexao1 = "localhost";
+// $database_conexao1 = "smarthouse";
+// $username_conexao1 = "root";
+// $password_conexao1 = "";
 
-// $hostname_conexao1 = "54.39.75.7";
-// $database_conexao1 = "bdqyngbnbsudmj189t37";
-// $username_conexao1 = "usrxplt5d3lv7sfi";
-// $password_conexao1 = "BhkiXAfdcVRh3q55Paou";
+include_once("secrets.php");
 
 $con1 = mysqli_connect($hostname_conexao1, $username_conexao1, $password_conexao1);
 if (!$con1)
