c0d8b8b95203b5b9fa97c8a84c63dac348b00038
casa <eltonbarbosagomes@gmail.com>
2021-12-21 06:58:13 -0300

    basic code

---
+++ casa_expansao/casa_expansao.ino
@@ -0,0 +1,200 @@
+/********************************************************************************\
+ * Programa Expansão de Entradas e saídas do Arduino, utilizando Shift Register *
+ * CI utilizado: 74HC165 com 74HC595                                            *
+ * Por: Elton Barbosa Gomes                                                     *
+ * Data: 28/09/2020                                                             *
+ * Créditos: Baseado no playground.arduino.cc                                   *
+\********************************************************************************/
+
+//74hc165
+// Definições de Labels
+#define nCIs  3               //Registra o número de CIs cascateados
+#define BYTES 8
+#define TempoDeslocamento 50  //Registra o tempo de que deverá ter o pulso para leitura e gravação, (milesegundos)
+#define Atraso  100           //Registra o atraso de segurança entre leituras, (milesegundos)
+
+// Declaração de constantes globais 165
+const byte ploadPin165        = 6;    //Conecta ao pino 1 do 74HC165 (LH/LD - asynchronous parallel load input)(PL)
+const byte clockEnablePin165  = 5;    //Conecta ao pino 15 do 74HC165 (CE - Clock Enable Input)(CE)
+const byte dataPin165         = 8;   //Conecta ao pino 9 do 74HC165 (Q7 - serial output from the last stage)(Q7)
+const byte clockPin165        = 7;   //Conecta ao pino 2 do 74HC165 (CP - Clock Input)(CP)
+
+//inicialização das variaveis onde serão armazenados os status  
+byte pinValues[nCIs];
+byte oldPinValues[nCIs];
+byte pinValuesOut[nCIs];
+byte oldPinValuesOut[nCIs];
+
+// Declaração de constantes globais 595
+const byte clockPin595 = 2; //Pino conectado a SRCLK (pino 11 no 74HC595), registrador de deslocamento
+const byte latchPin595 = 3; //Pino conectado a RCLK (pino 12 no 74HC595), registrador de armazenamento
+const byte dataPin595 = 4; //Pino conectado a SER (pino 14 no 74HC595), entrada de dados serial
+
+//usado para contar o tempo do botao pressionado
+long time;
+byte pinKey[nCIs];
+
+//Função para definir um rotina shift-in, lê os dados do 74HC165
+void read_shift_regs(){
+    bool bitVal=0; //variavel para que armazena bit a bit
+    byte bytesVal[nCIs];
+    byte bytesValOut[nCIs];
+    for(int i = 0; i < nCIs; i++){
+        bytesVal[i] = 0;
+        bytesValOut[i] = pinValuesOut[i];
+
+    }
+
+    //desloca todos os bits para o pino de dados para leitura
+    digitalWrite(clockEnablePin165, HIGH);
+    digitalWrite(ploadPin165, LOW);
+    delayMicroseconds(TempoDeslocamento);
+    digitalWrite(ploadPin165, HIGH);
+    digitalWrite(clockEnablePin165, LOW);
+
+    // Efetua a leitura de um bit da saida serial do 74HC165
+
+    for(int i = 0; i < nCIs; i++){
+        for (int j = 0; j < BYTES; j++){
+            bitVal = digitalRead(dataPin165);
+
+            //Realiza um shift left e armazena o bit correspondente em bytesVal
+            bytesVal[nCIs-1-i] |= (bitVal << ((BYTES-1) - j));
+
+            //compara se ouve mudanca de borda de subida para alterar o 595
+            bytesValOut[nCIs-1-i] ^= (bitVal << ((BYTES-1) - j));
+
+            //Lança um pulso de clock e desloca o próximo bit
+            digitalWrite(clockPin165, HIGH);
+            delayMicroseconds(TempoDeslocamento);
+            digitalWrite(clockPin165, LOW);
+        }
+        pinValues[nCIs-1-i] = bytesVal[nCIs-1-i];
+
+        //condição para somente alterar as saídas se soltar o botão
+        if(pinValues[nCIs-1-i] != oldPinValues[nCIs-1-i]){
+            pinValuesOut[nCIs-1-i] = bytesValOut[nCIs-1-i];
+        }
+
+        //condição para desligar apos um tempo que o botao ficar pressionado
+        if((pinValues[nCIs-1-i] != 0) && (pinKey[nCIs-1-i] == 0)){
+            time = millis();
+            pinKey[nCIs-1-i] = pinValues[nCIs-1-i];
+        }
+
+        //condicao para impedir o desligamento com o botao sem pressionar e resetar o pinKey
+        if(pinValues[nCIs-1-i] == 0){
+            pinKey[nCIs-1-i] = 0;
+        }
+
+        //condiçao para funcionar apenas com o botao 04
+        if(((millis() - time) > 2000) && (pinKey[nCIs-1-i] == 8) && ((nCIs-1-i) == 0)){
+            Serial.print("\nElton\n");
+            Serial.print(pinValuesOut[nCIs-1-i]);
+            pinValuesOut[nCIs-1-i] &= (0 << 4);
+            Serial.print("\n");
+            Serial.print(pinValuesOut[nCIs-1-i]);
+            Serial.print("\n");
+        }
+
+        //desliga as saidas
+        if(((millis() - time) > 4000) && (pinKey[nCIs-1-i] != 0)){
+            pinValuesOut[nCIs-1-i] = 0;
+        }
+    }
+}
+
+void alteraSaida(){
+    //alterar saida do 595
+    for (int i = 0; i < nCIs; ++i){
+        digitalWrite(latchPin595, LOW);
+        shiftOut(dataPin595, clockPin595, LSBFIRST, pinValuesOut[i]);
+        digitalWrite(latchPin595, HIGH);
+    }
+}
+
+//Mostra os dados recebidos
+void display_pin_values()
+{
+    Serial.print("Estado das entradas:\r\n");
+
+    for(int i = 0; i < nCIs; i++){
+        for(int j = 0; j < BYTES; j++){
+            Serial.print("  Pin0-");
+            Serial.print(i);
+            Serial.print(j);
+            Serial.print(": ");
+
+            if((pinValues[i] >> j) & 1)
+                Serial.print("ALTO");
+            else
+                Serial.print("BAIXO");
+
+            Serial.print("\r\n");
+        }
+    }
+
+    Serial.print("\r\n");
+}
+
+// Configuração do Programa
+void setup()
+{
+    //habilita a comunicação via monitor serial
+    Serial.begin(9600);
+    
+    //Inicializa e configura os pinos do 165
+    pinMode(ploadPin165, OUTPUT);
+    pinMode(clockEnablePin165, OUTPUT);
+    pinMode(clockPin165, OUTPUT);
+    pinMode(dataPin165, INPUT);
+
+    digitalWrite(clockPin165, HIGH);
+    digitalWrite(ploadPin165, HIGH);
+
+    //incializa pinos do 595
+    pinMode(latchPin595, OUTPUT);
+    pinMode(clockPin595, OUTPUT);
+    pinMode(dataPin595, OUTPUT);
+
+    //Lê e mostra o estado dos pinos
+    for(int i = 0; i < nCIs; i++){
+        pinValues[i] = 0;
+        oldPinValues[i] = 0;
+        pinValuesOut[i] = 0;
+        oldPinValuesOut[i] = 0;
+        pinKey[i] = 0;
+    }
+
+    alteraSaida();
+    display_pin_values();
+}
+
+//Função do loop principal
+void loop(){
+    //Lê todos as portas externas
+
+    read_shift_regs();
+    
+    //Se houver modificação no estado dos pinos, mostra o estado atual
+    for(int i = 0; i < nCIs; i++){
+        if(pinValues[i] != oldPinValues[i]){
+            Serial.print("\n*Alteracao detectada*\r\n");
+            display_pin_values();
+            Serial.print("Valor das entradas em decimal:\n");
+            Serial.print(i);
+            Serial.print("\t");
+            Serial.print(pinValues[i]);
+            oldPinValues[i] = pinValues[i];
+        }
+    }
+    //altera a saída se existir alguma mudança
+    for(int i = 0; i < nCIs; i++){
+        if (oldPinValuesOut[i] != pinValuesOut[i]){
+            alteraSaida();
+            oldPinValuesOut[i] = pinValuesOut[i];
+        }
+    }
+
+    delay(Atraso);
+}
