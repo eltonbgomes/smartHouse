67fa8be28fd4fd225313384be31f899b577e54fd
casa <eltonbarbosagomes@gmail.com>
2022-01-30 18:10:15 -0300

    retirada funcao de gravar do master

--- casaExpansaoMaster/casaExpansaoMaster.ino
+++ casaExpansaoMaster/casaExpansaoMaster.ino
@@ -1,6 +1,5 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_Fingerprint.h>
-#include <PushButton.h>
 #include <A2a.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -8,141 +7,22 @@
 // DEFINIÇÃO DO ENDEREÇO DO SLAVE
 #define address 0x08
 
-//botao para cadastrar digital ADM
-#define pinButton 10
-
 // DEFINIÇÃO DO PINO DA TRAVA
-#define pinLock 2
-#define pinLockSensor 3
+#define pinLock 9
+#define pinLockSensor 10
 
 // INSTANCIANDO OBJETOS
 SoftwareSerial mySerial(11, 12); //pin 11 = Tx do sensor, pin 12 Rx do sensor
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
-PushButton button(pinButton);
 
-// DECLARAÇÃO DAS VARIÁVEIS E FUNCOES
-uint8_t numID = 0;
-bool save = false;
-
-OneWire oneWire(9);
+OneWire oneWire(2);
 DallasTemperature sensor(&oneWire);
  
 int nTempSensors = 0;
 float tempC;
 
 A2a arduinoSlave;
-
-uint8_t read(int p, int ID){
-    while (p != FINGERPRINT_OK) {
-        p = finger.getImage();
-        switch (p) {
-            case FINGERPRINT_OK:
-                Serial.println("Leitura concluída");
-                break;
-            case FINGERPRINT_NOFINGER:
-                Serial.println(".");
-                delay(200);
-                break;
-            case FINGERPRINT_PACKETRECIEVEERR:
-                Serial.println("Erro de comunicação");
-                break;
-            case FINGERPRINT_IMAGEFAIL:
-                Serial.println("Erro de leitura");
-                break;
-            default:
-                Serial.println("Erro desconhecido");
-                break;
-        }
-    }
-
-    p = finger.image2Tz(ID);
-    switch (p) {
-        case FINGERPRINT_OK:
-            Serial.println("Leitura convertida");
-            return 0;
-        case FINGERPRINT_IMAGEMESS:
-            Serial.println("Leitura suja");
-            return p;
-        case FINGERPRINT_PACKETRECIEVEERR:
-            Serial.println("Erro de comunicação");
-            return p;
-        case FINGERPRINT_FEATUREFAIL:
-            Serial.println("Não foi possível encontrar propriedade da digital");
-            return p;
-        case FINGERPRINT_INVALIDIMAGE:
-            Serial.println("Não foi possível encontrar propriedade da digital");
-            return p;
-        default:
-            Serial.println("Erro desconhecido");
-            return p;
-    }    
-}
-
-uint8_t saveModeID(uint8_t saveID) {
-
-    int p = -1;
-    Serial.print("Esperando uma leitura válida para salvar #");
-    Serial.println(saveID);
-    delay(2000);
-
-    uint8_t helpRead = read(p, 1);
-    if (helpRead != 0){
-        return helpRead;
-    }
-
-    Serial.println("Remova o dedo");
-    delay(2000);
-    p = 0;
-    while (p != FINGERPRINT_NOFINGER) {
-        p = finger.getImage();
-    }
-    Serial.print("ID ");
-    Serial.println(saveID);
-    p = -1;
-    Serial.println("Coloque o Mesmo dedo novamente");
-    
-    helpRead = read(p, 2);
-    if (helpRead != 0){
-        return helpRead;
-    }
-
-    // OK convertido!
-    Serial.print("Criando modelo para #");
-    Serial.println(saveID);
-  
-    p = finger.createModel();
-    if (p == FINGERPRINT_OK) {
-        Serial.println("As digitais batem!");
-    } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-        Serial.println("Erro de comunicação");
-        return p;
-    } else if (p == FINGERPRINT_ENROLLMISMATCH) {
-        Serial.println("As digitais não batem");
-        return p;
-    } else {
-        Serial.println("Erro desconhecido");
-        return p;
-    }
-  
-    Serial.print("ID "); Serial.println(saveID);
-    p = finger.storeModel(saveID);
-    if (p == FINGERPRINT_OK) {
-        Serial.println("Armazenado!");
-    } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-        Serial.println("Erro de comunicação");
-        return p;
-    } else if (p == FINGERPRINT_BADLOCATION) {
-        Serial.println("Não foi possível save neste local da memória");
-        return p;
-    } else if (p == FINGERPRINT_FLASHERR) {
-        Serial.println("Erro durante escrita na memória flash");
-        return p;
-    } else {
-        Serial.println("Erro desconhecido");
-        return p;
-    }
-}
 
 int getFingerprintIDez() {
     uint8_t p = finger.getImage();
@@ -164,6 +44,7 @@ int getFingerprintIDez() {
 }
 
 void setup() {
+    Serial.begin(9600);
 
     //configuracao do sensor biometrico
     pinMode(pinLock, OUTPUT);
@@ -187,7 +68,6 @@ void setup() {
     arduinoSlave.begin();
 
     sensor.begin(); 
-    Serial.begin(9600);
     Serial.println("Localizando Dispositivos ...");
     Serial.print("Encontrados ");
     nTempSensors = sensor.getDeviceCount();
@@ -202,6 +82,9 @@ void loop() {
         digitalWrite(pinLock, LOW);
         Serial.println("Porta Travada");
     }
+
+    getFingerprintIDez();
+
     Serial.print("\nStatus\n");
     for(int i = 0; i < 3; i++){
             Serial.print(arduinoSlave.varWireRead(address, i));
@@ -217,18 +100,4 @@ void loop() {
         Serial.print(tempC);
         Serial.println("ºC");
     }
-
-    button.button_loop();
-
-    if ( button.pressed() ){
-        save = true;
-    }
-
-    if(save){
-        saveModeID(numID);
-        numID++;
-        save = false;
-    }
-
-    getFingerprintIDez();
 }
